90f9a187ee152159ca4bb4af4bd819c02071b983
gtodur <gtodur@outlook.com>
2020-03-14 10:00:13 +0530

    First commit

---
+++ src/main/java/com/df/springboot/ldap/auth/SpringBootLdapAuthApplication.java
@@ -0,0 +1,13 @@
+package com.df.springboot.ldap.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLdapAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLdapAuthApplication.class, args);
+	}
+
+}
